b5a03129bd414a34c59004070eac78b1f9650405
diman-dragon <1448884@gmail.com>
2026-02-23 22:30:34 +0100

    fix: added pathPrefix for github pages

--- .eleventy.js
+++ .eleventy.js
@@ -4,15 +4,19 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/css");
   
-  // Явно указываем копирование JS, чтобы база layla-db.json была доступна
+  // Явно указываем копирование JS
   eleventyConfig.addPassthroughCopy("src/js");
 
-  // Следим за изменениями в JSON и CSS, чтобы сайт обновлялся сам
+  // Следим за изменениями
   eleventyConfig.addWatchTarget("./src/js/");
   eleventyConfig.addWatchTarget("./src/css/");
 
   return {
-    // Указываем Nunjucks как основной движок для HTML и Markdown
+    // ВАЖНО: Это название твоего репозитория на GitHub
+    // Если репозиторий называется linguadeck_ai, оставь так. 
+    // Если просто data_law_site — замени.
+    pathPrefix: "/linguadeck_ai/", 
+
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk",
@@ -21,8 +25,6 @@ module.exports = function(eleventyConfig) {
       input: "src",
       output: "public",
       includes: "_includes",
-      // Если у тебя есть папка для макетов (layout), укажи её, 
-      // иначе Eleventy ищет их внутри _includes
       layouts: "_includes" 
     }
   };
